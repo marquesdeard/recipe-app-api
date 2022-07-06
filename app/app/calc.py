6c81afdcc776348618cb6a8f29b8837634c627b7
marquesdeard <ekemaldemirel@gmail.com>
2022-07-06 22:02:59 +0300

    Git test :wq

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,10 +7,7 @@ Calculator functions
 def add(x, y):
     """Add x and y return result."""
     return x + y
- 
 
 def subtract(x, y):
     """Add x and y return result."""
     return abs(x - y)
- 
-
