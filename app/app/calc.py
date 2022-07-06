73034ab72a748eb484349557418aaafc01c7b3bf
marquesdeard <ekemaldemirel@gmail.com>
2022-07-06 21:50:09 +0300

    Configred docker compose and check to wait for db :wq

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+Calculator functions
+
+"""
+
+
+def add(x, y):
+    """Add x and y return result."""
+    return x + y
+ 
+
+def subtract(x, y):
+    """Add x and y return result."""
+    return abs(x - y)
+ 
+
